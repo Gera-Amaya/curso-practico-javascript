b931154f43eea2349b7bed6ee44f0078ed9eb954
Gera-Amaya <gamayamolia@outlook.com>
2021-12-04 00:26:18 -0700

    se agrego archivo html y js

---
+++ code.js
@@ -0,0 +1,30 @@
+
+//inicio de curso practico de JS
+
+
+/*
+
+var name = "Gera";
+var lastname = "Amaya";
+var completeName = "Gera Amaya";
+var nickname = "Jerry";
+
+
+console.log(`Mi nombre es ${completeName}, pero prefiero que me digas ${nickname}`); */
+
+//funcion de prueba
+/*
+function suma (number){
+    var number = prompt("cuanto es 2 mas 2?")
+    if(number == 4){
+    console.log("felicidades");
+    }else{
+        console.log("te has equivocado");
+    }
+}
+
+suma();
+*/
+
+
+
